6965bd13ee01cb577ea30fa106d7e39707363001
pboulet <pboul037@gmail.com>
2014-11-07 17:38:07 -0500

    updated client with backup folder

--- client/app/shell.js
+++ client/app/shell.js
@@ -5,7 +5,8 @@
         router: router,
         activate: function () {
             router.map([
-              { route: '', title: 'Home', moduleId: 'home', nav: true }
+              { route: '', title: 'Home', moduleId: 'home', nav: true },
+              { route:'dashboard', title:'Dashboard', moduleId:'dashboard', nav:true }
             ]).buildNavigationModel();
 
             return router.activate();
