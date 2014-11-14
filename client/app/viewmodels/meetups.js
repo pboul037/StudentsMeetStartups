07ff51945348b141e76dfd998cb942abe34a80be
pboulet <pboul037@gmail.com>
2014-11-14 10:47:30 -0500

    imported a custom stylesheet, write a review GUI

--- client/app/viewmodels/meetups.js
+++ client/app/viewmodels/meetups.js
@@ -7,6 +7,10 @@ define(['durandal/app', 'plugins/router', 'knockout'], function (app, router, ko
         },
         browse: function(){
             router.navigate('explore');
-        }
+        },
+        writeReview: function(item){
+            item.viewUrl = 'views/writeReviewModal';
+            app.showDialog(item);  
+        },
     };
 });
